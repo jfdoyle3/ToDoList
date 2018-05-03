6d19f8441d58e5e72d6f90b84c6df5ce08ce5dd8
jfdoyle3 <indellibleink@gmail.com>
2018-05-03 02:06:45 +0000

    v1.0

---
+++ todo_list.js
@@ -0,0 +1,17 @@
+//  ToDo List v1.0
+// It should have a place to store todos
+var todos=['item 1', 'item 2', 'item 3'];
+
+// It should have a way to display todos
+    console.log(todos);
+
+// It should have a way to add new todos
+    todos.push(todos);
+
+
+//It should have a way to change todos.
+    todos[0]= 'item 1 updated';
+
+
+// It should have a way to delete todos.
+    todos.splice(0, 1);
