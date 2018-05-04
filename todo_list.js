51d72e0c0e064daf1b4d5063c6d538cc2abdbd13
jfdoyle3 <indellibleink@gmail.com>
2018-05-04 16:44:24 +0000

    v4.0

--- todo_list.js
+++ todo_list.js
@@ -1,20 +1,28 @@
-//v3.0
+//v4.0
 
 var todosList={
-    todos: ['item 1', 'item 2', 'item 3'],
+    todos: [],
     displayTodos: function (){
         console.log("My Todos: ",this.todos);
     },
-    addTodo: function(todo){
-        this.todos.push(todo);
+    addTodo: function(todoText){
+        this.todos.push({
+            todoText: todoText,
+            completed: false
+            });
         this.displayTodos();
     },
-    changeTodo: function (position, newValue){
-        this.todos[position]=newValue;
+    changeTodo: function (position, todoText){
+        this.todos[position].todoText=todoText;
         this.displayTodos();
     },
     deleteTodo: function(position){
         this.todos.splice(position ,1);
         this.displayTodos();
-    }
+    },
+    toggleComplete: function(position){
+        var todo=this.todos[position];
+        todo.completed=!todo.completed;
+        this.displayTodos();
+    }      
 };
