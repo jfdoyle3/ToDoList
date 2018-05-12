4a6bc4a0bf5a9be559878a0a63a9b09468ad284b
jfdoyle3 <indellibleink@gmail.com>
2018-05-12 05:56:35 +0000

    updated code

--- todo_list.js
+++ todo_list.js
@@ -8,10 +8,15 @@ var todosList={
         } else {
            console.log("My Todos: ");
         for(var i=0; i < this.todos.length; i++){
-            console.log(this.todos[i].todoText);
-          }    
+              if (this.todos[i].completed === true){
+              console.log('(x)',this.todos[i].todoText);  
+           }
+                else {
+              console.log('( )',this.todos[i].todoText);
+                }
+            }
         }
-    },
+      },
     addTodo: function(todoText){
         this.todos.push({
             todoText: todoText,
